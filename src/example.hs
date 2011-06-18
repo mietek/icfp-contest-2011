9fbb7df490b4345ceb069086ce04890a0e520053
Miëtek Bak <mietek@varsztat.com>
2011-06-18 14:42:42 +0100

    Fix indentation in example player

--- src/example.hs
+++ src/example.hs
@@ -5,30 +5,30 @@ import System.IO (BufferMode (..), hSetBuffering, stdin, stdout)
 
 main :: IO ()
 main = do
-    hSetBuffering stdin LineBuffering
-    hSetBuffering stdout LineBuffering
-    playerId <- fmap head getArgs
-    if playerId == "1"
-        then getOpponentsMove
-        else putMove
+  hSetBuffering stdin LineBuffering
+  hSetBuffering stdout LineBuffering
+  playerId <- fmap head getArgs
+  if playerId == "1"
+    then getOpponentsMove
+    else putMove
 
 putMove :: IO ()
 putMove = do
-    putStrLn "1"
-    putStrLn "I"
-    putStrLn "0"
-    getOpponentsMove
+  putStrLn "1"
+  putStrLn "I"
+  putStrLn "0"
+  getOpponentsMove
 
 getOpponentsMove :: IO ()
 getOpponentsMove = do
-    moveType <- getLine
-    (card, slot) <- case moveType of
-        "1" -> do
-            card <- getLine
-            slot <- getLine
-            return (card, slot)
-        "2" -> do
-            slot <- getLine
-            card <- getLine
-            return (card, slot)
-    putMove
+  moveType <- getLine
+  (card, slot) <- case moveType of
+    "1" -> do
+      card <- getLine
+      slot <- getLine
+      return (card, slot)
+    "2" -> do
+      slot <- getLine
+      card <- getLine
+      return (card, slot)
+  putMove
