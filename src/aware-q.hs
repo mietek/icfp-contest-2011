5387b7be1d4805eaae97961b5fc5df05a81097d3
Kuba <jmi@jmi-virt.(none)>
2011-06-19 23:35:54 +0200

    Wypychanie na żądanie.

--- src/aware-q.hs
+++ src/aware-q.hs
@@ -19,16 +19,21 @@ import Common
 mainProgram :: Program
 mainProgram =
   Concat [
-    Move (ApplyR 1 Zero),
-    Replicate 256 [
-      Replicate 10000 [
-        Move (ApplyR 0 Zero),
-        Move (ApplyL Succ 0),
-        Move (ApplyL Get 0),
-        Move (ApplyL Dec 0)
-      ],
-      Move (ApplyL Succ 1)
-    ]
+	 setNumber 9999 3,
+	 setNumber (10000+9999*11 `div` 10-1) 5,
+	 setNumber 0 1,
+	 setNumber 1 2,
+         valueToProgram (FunctionValue (SFunction2 (lazyHelp 1 2 3) (lazyHelp 2 1 5))) 4,
+         valueToProgram (FunctionValue (SFunction2 (lazyAttack 1 1 7) (lazyHelp 1 2 7))) 6,
+	 setNumber 11112 7,
+	 Replicate 128
+		 [ copyFrom 6 0,
+		 activate 0,
+		 Move (ApplyL Succ 1),
+		 Move (ApplyL Succ 1),
+		 Move (ApplyL Succ 2),
+		 Move (ApplyL Succ 2)],
+         Replicate 10000 [Move (ApplyL I 0)]
   ]
 
 main :: IO ()
