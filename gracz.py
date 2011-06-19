8c1716064a2b159bbaead8be0882b82d3c3f167d
jmi.wroc <mijaczas@gmail.com>
2011-06-19 08:19:48 +0200

    poprawa efektywnoscipoprawa efektywnoscipoprawa efektywnoscipoprawa efektywnoscipoprawa efektywnoscipoprawa efektywnoscipoprawa efektywnoscipoprawa efektywnoscipoprawa efektywnoscipoprawa efektywnosci

--- gracz.py
+++ gracz.py
@@ -22,7 +22,7 @@ def slot(t, t2):
     print t2    
    
 def help(cart, params): 
-    slotRev = random.randint(32, 140)
+    slotRev = random.randint(50, 140)
     slot(slotRev, cart)
     
     for p in params:
@@ -42,31 +42,40 @@ def setInt(slotNr, value):
 def myCeil(x, y):
     return int(math.ceil(float(x)/float(y)))
 
-def newHelp(cart, params):
-        slot(slotW, cart)
-
-        for p in params:        
-            card("K", slotW)
-            card("S", slotW)
-            slot(slotW, "get")
+def newHelp(cart, params, dest):
+        slot(dest, cart)
+        i=0
+        for p in params:
+            card("K", dest)
+            card("S", dest)
+            slot(dest, "get")
             for t in toCards(p):
-                card("K", slotW)
-                card("S", slotW)
-                slot(slotW, t)
-            slot(slotW, "zero")
+                card("K", dest)
+                card("S", dest)
+                slot(dest, t)
+            if i<2: 
+                card("K", dest)
+                card("S", dest)
+                card("K", dest)
+                card("S", dest)
+                slot(dest, "K")
+                slot(dest, "zero")
+                card("S", dest)
+                i=i+1
+                slot(dest, "I")
 
-slotW = 9
-slotL1 = 12
-slotL2 = 14
-slotL3 = 16
-slotA = 17
+slotW = 5
+slotL1 = 6
+slotL2 = 7
+slotL3 = 8
+slotA = 9
 
-slotH1=32
-slotH2=33
+slotH1=10
+slotH2=11
 setInt(slotH1, 0)
 setInt(slotH2, 1)
-slotA1=34
-slotA2=35
+slotA1=12
+slotA2=13
 setInt(slotA1, 3)
 setInt(slotA2, 0)
 
@@ -81,38 +90,44 @@ setInt(slotL3, vL3)
 
 setInt(slotA, 11112)
 
-for z in range(64):
-    newHelp("help", [slotH1, slotH2, slotL1])
-    card("succ", slotH1)
-    card("succ", slotH2)
-    newHelp("help", [slotH1, slotH2, slotL2])
-    card("succ", slotH1)
-    card("succ", slotH2)
-    newHelp("help", [slotH1, slotH2, slotL3])
-    card("succ", slotH1)
-    card("succ", slotH2)
-    card("succ", slotH1)
-    card("succ", slotH2)
-    newHelp("attack", [slotA1, slotA2,  slotA])
-    card("succ", slotA2)
-    newHelp("attack", [slotA1, slotA2,  slotA])
-    card("succ", slotA2)
-    newHelp("attack", [slotA1, slotA2,  slotA])
-    card("succ", slotA2)
-    newHelp("attack", [slotA1, slotA2,  slotA])
-    card("succ", slotA2)
-    card("succ", slotA1)
-    card("succ", slotA1)
-    card("succ", slotA1)
-    card("succ", slotA1)
-    # help("help", [0+2*z, 1+2*z, 9999])
-    # help("help", [1+2*z, 0+2*z, 20997])
-    # help("attack", [0+2*z, 0+2*z, 11112])
-    # help("attack", [0+2*z, 1+2*z, 11112])
+newHelp("help", [slotH1, slotH2, slotL1], 2)
+newHelp("help", [slotH1, slotH2, slotL2], 3)
+newHelp("help", [slotH1, slotH2, slotL3], 4)
+newHelp("attack", [slotA1, slotA2, slotA], 1)
 
-# while True:
-    # for k in range(256):
-        # help("revive", [k])
+def execute(slotnr):
+    setInt(35, slotnr)
+    card("get", 35)
+    slot(35, "zero")
+    
+for z in range(64):
+    execute(2)
+    card("succ", slotH1)
+    card("succ", slotH2)
+    execute(3)
+    card("succ", slotH1)
+    card("succ", slotH2)
+    execute(4)
+    card("succ", slotH1)
+    card("succ", slotH2)
+    card("succ", slotH1)
+    card("succ", slotH2)
+    execute(1)
+    card("succ", slotA2)
+    execute(1)
+    card("succ", slotA2)
+    execute(1)
+    card("succ", slotA2)
+    execute(1)
+    card("succ", slotA2)
+    card("succ", slotA1)
+    card("succ", slotA1)
+    card("succ", slotA1)
+    card("succ", slotA1)
+
+while True:
+    for k in range(256):
+        help("revive", [k])
         
 
 
